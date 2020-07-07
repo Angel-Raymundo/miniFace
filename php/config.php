9520e46b96b0b459def726c817b53d566033648b
Angel-Raymundo <jimray214@gmail.com>
2020-07-06 21:16:20 -0500

    conexion a base de datos

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php 
 	$server="localhost";
-    $user="root";
-	$pass="";
-	$bd="blog";
+    $user="ray_miniface";
+	$pass="r4yM1n1f4c3";
+	$bd="ray_miniface";
 
 	$cx=mysqli_connect($server,$user,$pass,$bd);
 

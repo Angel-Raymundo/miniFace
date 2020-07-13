dce9e48e7c381c85ff840fba918c2b6d28bd4171
Angel-Raymundo <jimray214@gmail.com>
2020-07-12 21:07:53 -0500

    refactorice todos los html

--- README.md
+++
@@ -1,5 +0,0 @@
-# miniFace
-Es un proyecto escolar el cual es una red social, la cual permite agregar comentarios publicos para todos los usuarios de esta web
-permite observar todos los comentarios realizados por el mismo usuario
-permite cambiar la informacion del perfil
-requiere una base de datos la cual se puede enlazar en assets/php/config.php
--- index.php
+++ index.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <title>Red Social con PHP</title>
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/estilinis.css">
   <link rel="stylesheet" href="assets/css/estilos.css">

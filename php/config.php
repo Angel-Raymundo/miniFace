edc582b7745d791f3e046c72c83bd65e7a68ccc1
Alberto Maturano <jose.maturano@globant.com>
2020-07-12 22:58:40 -0500

    Fix avoid using sensible information on code
    Uses `.env` file to manage sensible information through enviroment
    variables

--- php/config.php
+++ php/config.php
@@ -1,11 +1,12 @@
 <?php 
-	$server="localhost";
-    $user="ray_miniface";
-	$pass="r4yM1n1f4c3";
-	$bd="ray_miniface";
+require('vendor/autoload.php');
 
-	$cx=mysqli_connect($server,$user,$pass,$bd);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 
+$server = $_ENV['DB_SERVER'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$bd = $_ENV['DB_NAME'];
 
-
- ?>
+$cx = mysqli_connect($server, $user, $pass, $bd);
